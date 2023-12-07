5a153af0528502b9e4d02f594cb9f04da9566483
athi-satheesh <athisatheesh303@gmail.com>
2023-12-07 14:17:13 +0530

    CRUD

--- new_app/urls.py
+++ new_app/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from new_app import views
 
 urlpatterns = [
-    path('', views.read, name="read"),
+    path('', views.read, name=""),
+    path('create', views.create, name="create"),
+    path('updt/<int:id>/', views.update, name="updt"),
     path('delt/<int:id>/', views.delete, name="delt"),
 ]
--- new_app/views.py
+++ new_app/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render, redirect
 
+from new_app.forms import EmployeeForm
 from new_app.models import Employee
 
 
 # Create your views here.
 
+# create-date
+def create(request):
+    form = EmployeeForm()
+    if request.method == "POST":
+        form1 = EmployeeForm(request.POST)
+        if form1.is_valid():
+            form1.save()
+            return redirect("")
+        else:
+            form = form1
+    return render(request, "addEmployee.html", {"form": form})
 
+
+# read-data
 def read(request):
     data = Employee.objects.all()
     return render(request, "index.html", {"data": data})
+
+
+# update-data
+def update(request, id):
+    emp = Employee.objects.get(id=id)
+    form = EmployeeForm(instance=emp)
+    if request.method == "POST":
+        form1 = EmployeeForm(request.POST, instance=emp)
+        if form1.is_valid():
+            form1.save()
+            return redirect("")
+    return render(request, "updateEmployee.html", {'form': form})
 
 
 # delete-data
@@ -16,5 +42,5 @@ def delete(request, id):
     if request.method == 'POST':
         delt = Employee.objects.get(id=id)
         delt.delete()
-        return redirect("read")
+        return redirect("")
     return render(request, "index.html")
